f6de369137651d65f38e8c47b66c96555e7a0b45
4bmaus <111831442+4bmaus@users.noreply.github.com>
2024-12-15 14:07:33 -0800

    Update main.c

--- main.c
+++ main.c
@@ -1,21 +1,21 @@
+#include "webserver.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "webserver.h"
 
 int main(int argc, char *argv[]) {
-    int port = 80; // Default port
+  int port = 80; // Default port
 
-    // Check for "-p" option to specify a different port
-    if (argc > 2 && strcmp(argv[1], "-p") == 0) {
-        port = atoi(argv[2]);
-        if (port <= 0) {
-            fprintf(stderr, "Invalid port number.\n");
-            return 1;
-        }
+  // Check for "-p" option to specify a different port
+  if (argc > 2 && strcmp(argv[1], "-p") == 0) {
+    port = atoi(argv[2]);
+    if (port <= 0) {
+      fprintf(stderr, "Invalid port number.\n");
+      return 1;
     }
+  }
 
-    printf("Starting server on port %d\n", port);
-    start_server(port);
-    return 0;
+  printf("Starting server on port %d\n", port);
+  start_server(port);
+  return 0;
 }
